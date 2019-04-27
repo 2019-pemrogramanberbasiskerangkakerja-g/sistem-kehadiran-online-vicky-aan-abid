a18b75ab1fd5cdb794982f7da36ba5b11e518200
vickymahfudy <vicky15@mhs.if.its.ac.id>
2019-04-28 02:53:04 +0700

    Halaman login

---
+++ login.js
@@ -0,0 +1,52 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'db_absensi'
+});
+
+connection.connect();
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM admin WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.send('Sukses!');
+			} else {
+				response.send('Gagal!');
+			}			
+			response.end();
+		});
+
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.listen(3000,function(){
+    console.log("I am live at PORT 3000.");
+});
